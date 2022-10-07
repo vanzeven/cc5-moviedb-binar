47fb1d2f00e24c4b299b6fb12d2e3160fc928083
Faisal Reza <7farema@gmail.com>
2022-10-07 19:20:04 +0700

    add skeleton for profile

--- app/src/main/java/com/example/moviedb/service/ApiService.kt
+++ app/src/main/java/com/example/moviedb/service/ApiService.kt
@@ -3,8 +3,11 @@ package com.example.moviedb.service
 import com.example.moviedb.model.GetPopularItem
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
-    @GET("movie/popular?api_key=6cf7c98d8b65a3bc61f70dd2f72accf2")
-    fun getPopular(): Call<List<GetPopularItem>>
+    @GET("movie/popular")
+    fun getPopular(
+        @Query("api_key") api_key: String = "6cf7c98d8b65a3bc61f70dd2f72accf2"
+    ): Call<List<GetPopularItem>>
 }
