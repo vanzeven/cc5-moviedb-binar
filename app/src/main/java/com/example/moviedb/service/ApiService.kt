b5549114a6f06a16c6814f3271e9606c1d9f3a8e
Faisal Reza <7farema@gmail.com>
2022-10-07 19:38:12 +0700

    api pixabay otw

--- app/src/main/java/com/example/moviedb/service/ApiService.kt
+++ app/src/main/java/com/example/moviedb/service/ApiService.kt
@@ -1,13 +1,44 @@
 package com.example.moviedb.service
 
 import com.example.moviedb.model.GetPopularItem
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.concurrent.TimeUnit
 
 interface ApiService {
     @GET("movie/popular")
-    fun getPopular(
-        @Query("api_key") api_key: String = "6cf7c98d8b65a3bc61f70dd2f72accf2"
-    ): Call<List<GetPopularItem>>
+    fun getPopular(): Call<List<GetPopularItem>>
+
+    companion object {
+
+        @JvmStatic
+        operator fun invoke(): ApiService {
+            val authInterceptor = Interceptor {
+                val originRequest = it.request()
+                val oldUrl = originRequest.url
+                val newUrl = oldUrl.newBuilder().apply {
+                    addQueryParameter("api_key", "6cf7c98d8b65a3bc61f70dd2f72accf2")
+                }.build()
+                it.proceed(originRequest.newBuilder().url(newUrl).build())
+            }
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(authInterceptor)
+                .connectTimeout(120, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/3/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build()
+                .create(ApiService::class.java)
+        }
+    }
 }
